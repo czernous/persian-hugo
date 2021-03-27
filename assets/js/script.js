c871e9f715a8df25899404f1c62de8b0c911a676
czernous-devexpress <anton.chernous@devexpress.com>
2021-03-27 13:58:48 +0300

    tweak styles and optimize css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,19 @@
-// Preloader js    
+// Preloader js
 $(window).on('load', function () {
-	$('.preloader').fadeOut(100);
+  $('.preloader').fadeOut(100);
 });
 
 (function ($) {
-	'use strict';
+  'use strict';
 
-
-	//  Search Form Open
-	$('#searchOpen').on('click', function () {
-		$('.search-wrapper').addClass('open');
-		setTimeout(function () {
-			$('.search-box').focus();
-		}, 400);
-	});
-	$('#searchClose').on('click', function () {
-		$('.search-wrapper').removeClass('open');
-	});
-
+  //  Search Form Open
+  $('#searchOpen').on('click', function () {
+    $('.search-wrapper').addClass('open');
+    setTimeout(function () {
+      $('.search-box').focus();
+    }, 400);
+  });
+  $('#searchClose').on('click', function () {
+    $('.search-wrapper').removeClass('open');
+  });
 })(jQuery);
